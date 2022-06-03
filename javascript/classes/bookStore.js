3d8ed5b23f9fc417956a28fc7d662019097b72a5
Rockwell70 <rockwellshabani@gmail.com>
2022-06-03 09:42:03 -0700

    media project

--- javascript/classes/bookStore.js
+++ javascript/classes/bookStore.js
@@ -29,3 +29,56 @@ class Media {
 
     }
 }
+
+class Book extends (Media) {
+    constructor(title, author, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+}
+
+class Movie extends (Media) {
+    constructor(title, director, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+
+    get director() {
+        return this._director;
+    }
+
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+class CD extends (Media) {
+    constructor(title, artist, songs) {
+        super(title);
+        this._artist = artist;
+        this._songs = songs;
+    }
+
+    get artist() {
+        return this._artist;
+    }
+
+    get songs() {
+        return this._songs;
+    }
+}
+
+const newMovie = new Movie('William B', 'Sammy', 98);
+
+newMovie.toggleCheckOutStatus(true);
+console.log(newMovie.isCheckOut);
