83cf778362853fd81ec8c7eb3d2b7262235cd615
Rockwell70 <rockwellshabani@gmail.com>
2022-06-06 12:59:19 -0700

    school project

--- javascript/classes/school.js
+++ javascript/classes/school.js
@@ -7,7 +7,7 @@ class School {
     constructor(name, level) {
         this._name = name;
         this._level = level;
-        this._numberOfStudents = [];
+        this._numberOfStudents = 0;
     }
 
     get name() {
@@ -17,88 +17,32 @@ class School {
     get level() {
         const levels = ['primary', 'middle', 'high'];
         return levels.includes(this._level) ?
-            this._level :
-            console.log('Incorrect level')
+            this._level : new Error ('Incorrect level');
     }
 
     get numberOfStudents() {
         return this._numberOfStudents;
     }
 
-    addRating(rating) {
-        this._numberOfStudents.push(rating);
+    set numberOfStudents(num) {
+        return num ?
+            this._numberOfStudents = num :
+            this._numberOfStudents;
     }
 
-    toggleCheckOutStatus() {
-        this._isCheckOut = !this._isCheckOut;
-    }
-
-    getAverageRating() {
-        const sumnumberOfStudents = this.numberOfStudents.reduce((previousValue, currentValue) => {
-            return previousValue + currentValue;
-        }, 0)
-
-        return (sumnumberOfStudents / this.numberOfStudents.length).toFixed(1);
-
-    }
-
-    getBestRating() {
-        return Math.max.apply(null, this.numberOfStudents);
+    quickFacts() {
+        console.log(`${this._name} is a ${this._level} school with ${this._numberOfStudents} students.`);
     }
 }
 
-class Book extends (School) {
-    constructor(name, author, pages) {
-        super(name);
-        this._author = author;
-        this._pages = pages;
-    }
-
-    get author() {
-        return this._author;
-    }
-
-    get pages() {
-        return this._pages;
-    }
-}
-
-class Movie extends (School) {
-    constructor(name, director, runTime) {
-        super(name);
-        this._director = director;
-        this._runTime = runTime;
-    }
-
-    get director() {
-        return this._director;
-    }
-
-    get runTime() {
-        return this._runTime;
-    }
-}
-
-class CD extends (School) {
-    constructor(name, artist, songs) {
-        super(name);
-        this._artist = artist;
-        this._songs = songs;
-    }
-
-    get artist() {
-        return this._artist;
-    }
-
-    get songs() {
-        return this._songs;
-    }
-}
 
 const school = new School('William B', 'low');
 console.log(school.level);
-const newMovie = new Movie('Speed', 'Jan de Bont', 116);
-const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
+console.log(school.numberOfStudents);
+school.numberOfStudents = 2300;
+school.quickFacts();
+
+
 
 //
 // historyOfEverything.toggleCheckOutStatus();
