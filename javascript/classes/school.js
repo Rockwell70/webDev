a0400581717a8d26562881cdf6cbec45243d241d
Rockwell70 <rockwellshabani@gmail.com>
2022-06-07 05:54:47 -0700

    added max rating.

--- javascript/classes/school.js
+++ javascript/classes/school.js
@@ -21,10 +21,13 @@ class School {
             console.log('Incorrect level: primary, middle, or high');
     }
 
+    /**@return {number}*/
     get numberOfStudents() {
         return this._numberOfStudents;
     }
 
+    //**@param {number} number of students to add
+    // @return {number}* number of students entered/
     set numberOfStudents(num) {
         this._numberOfStudents = num;
     }
@@ -34,10 +37,42 @@ class School {
         console.log(`Quick Facts:\nName: ${this._name}\nLevel: ${this._level}\nStudents: ${this._numberOfStudents}`);
 
     }
+
+    static pickSubstituteTeacher() {
+
+    }
 }
+
+/**A subclass of School.
+ * Has additional property: pickupPolicy*/
+class Primary extends (School) {
+    constructor(name, level, pickupPolicy) {
+        super(name, level);
+        /** @param {string=} pickupPolicy */
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+
+}
+
+/**A subclass of School with no additional
+ * methods or properties.*/
+class Middle extends (School) {
+    constructor(name, level) {
+        super(name, level);
+    }
+}
+
 const school = new School('William B', 'middle');
 console.log(school);
 console.log(school.level);
 school.numberOfStudents = 90;
 console.log(school.numberOfStudents);
 school.quickFacts();
+
+const p = new Primary('Sammy', 'low', 'after 3');
+console.log(p.pickupPolicy);
+
