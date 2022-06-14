87984c7846f07fdf7299706b48e203185bcc9d6e
Rockwell70 <rockwellshabani@gmail.com>
2022-06-14 06:10:21 -0700

    school classes work

---
+++ javascript/checkio/beginZeros.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+
+/** @param {string} text
+ * @return {number} Number of leading zeros in text.
+ */
+
+function beginningZeros(text){
+    // your code here
+    return 0;
+}
+
+console.log('Example:');
+console.log(beginningZeros('100'));
+
+// These "asserts" are used for self-checking
+assert.equal(beginningZeros('100'), 0);
+assert.equal(beginningZeros('001'), 2);
+assert.equal(beginningZeros('100100'), 0);
+assert.equal(beginningZeros('001001'), 2);
+assert.equal(beginningZeros('012345679'), 1);
+assert.equal(beginningZeros('0000'), 4);
+
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
--- javascript/checkio/splitPairs.js
+++ javascript/checkio/splitPairs.js
@@ -1,22 +1,26 @@
-//TODO: use substring & concat
+const assert = require("assert");
 
+/** @param {string} text */
 function splitPairs(text) {
     let result = [];
-    if (text.length % 2 !== 0) {
-        text = text.concat('_');
-    }
-    return text
+    //if text length is odd append "_" to the end
+    text.length  % 2 !== 0 ?
+        text = text.concat('_') :
+        text
+    for (let i = 0; i < text.length; i +=2)
+        result.push(text.substring(i, i + 2))
+    return result
 
 }
 console.log(splitPairs('abcdg'));
 // console.log('Example:');
-// console.log(splitPairs('abcd'));
-//
+console.log(splitPairs('abcd'));
+
 // // These "asserts" are used for self-checking
-// assert.deepEqual(splitPairs('abcd'), ['ab', 'cd']);
-// assert.deepEqual(splitPairs('abc'), ['ab', 'c_']);
-// assert.deepEqual(splitPairs('abcdf'), ['ab', 'cd', 'f_']);
-// assert.deepEqual(splitPairs('a'), ['a_']);
-// assert.deepEqual(splitPairs(''), []);
+assert.deepEqual(splitPairs('abcd'), ['ab', 'cd']);
+assert.deepEqual(splitPairs('abc'), ['ab', 'c_']);
+assert.deepEqual(splitPairs('abcdf'), ['ab', 'cd', 'f_']);
+assert.deepEqual(splitPairs('a'), ['a_']);
+assert.deepEqual(splitPairs(''), []);
 //
-// console.log("Coding complete? Click 'Check' to earn cool rewards!");
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
