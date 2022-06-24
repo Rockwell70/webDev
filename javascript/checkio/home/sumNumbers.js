a70a6748e7265d20d0e018710ac11f0454a3c270
Rockwell70 <rockwellshabani@gmail.com>
2022-06-24 14:26:08 -0700

    school project

--- javascript/checkio/home/sumNumbers.js
+++ javascript/checkio/home/sumNumbers.js
@@ -3,9 +3,10 @@ const assert = require("assert");
 function sumNumbers(test){
     let total = 0;
     //match groups of consecutive digits
-    const matched = test.match(/(\d[^a-z])+/g);
+    const matched = test.match(/(\d+)+(\b)/g);
+    //const matched = test.match(/(\d)+/g);
     //test of no digits found
-    if (! matched) {
+    if (! matched || !test) {
         return total;
     } else {
         /**if digits found iterate each string of digits
@@ -21,15 +22,16 @@ function sumNumbers(test){
 
 console.log('Example:');
 console.log(sumNumbers('my numbers is 2'));
+console.log(sumNumbers('Who is 1st here'));
 
 // // These "asserts" are used for self-checking
 assert.equal(sumNumbers('hi'), 0);
 assert.equal(sumNumbers('who is 1st here'), 0);
 assert.equal(sumNumbers('my numbers is 2'), 2);
-// assert.equal(sumNumbers('This picture is an oil on canvas '
-//     + 'painting by Danish artist Anna '
-//     + 'Petersen between 1845 and 1910 year'), 3755);
-// assert.equal(sumNumbers('5 plus 6 is'), 11);
-// assert.equal(sumNumbers(''), 0);
-//
+assert.equal(sumNumbers('This picture is an oil on canvas '
+    + 'painting by Danish artist Anna '
+    + 'Petersen between 1845 and 1910 year'), 3755);
+assert.equal(sumNumbers('5 plus 6 is'), 11);
+assert.equal(sumNumbers(''), 0);
+
 // console.log("Coding complete? Click 'Check' to earn cool rewards!");
