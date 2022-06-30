41083acd3d533f638d23bf2f4a217ad25bcc046b
Rockwell70 <rockwellshabani@gmail.com>
2022-06-30 06:34:05 -0700

    promises work

---
+++ javascript/checkio/home/backwardEachWord.js
@@ -0,0 +1,27 @@
+
+
+function backwardStringByWord(text) {
+    const temp = text.split(' ');
+    let newHolder = [];
+    let newText = '';
+    for (let i = 0; i < temp.length; i++){
+        for (let j = temp[i].length-1; j >= 0; j--) {
+            newText += temp[i][j];
+        }
+    } newHolder.push(`${newText} `);
+
+    return newHolder;
+}
+console.log(backwardStringByWord(text));
+
+console.log('Example:');
+console.log(backwardStringByWord(''));
+
+// These "asserts" are used for self-checking
+assert.equal(backwardStringByWord(''), '');
+assert.equal(backwardStringByWord('world'), 'dlrow');
+assert.equal(backwardStringByWord('hello world'), 'olleh dlrow');
+assert.equal(backwardStringByWord('hello   world'), 'olleh   dlrow');
+assert.equal(backwardStringByWord('welcome to a game'), 'emoclew ot a emag');
+
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
