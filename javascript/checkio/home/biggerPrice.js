08a44395923a75258c04e20a93499a27a0f526ba
Rockwell70 <rockwellshabani@gmail.com>
2022-07-02 11:47:25 -0700

    biggest price checkio

--- javascript/checkio/home/biggerPrice.js
+++ javascript/checkio/home/biggerPrice.js
@@ -2,13 +2,10 @@ const {deepEqual} = require("assert");
 
 
 function biggerPrice(limit, data) {
-    let priceHolder = [];
-    let total = 0;
-    data.forEach(item => priceHolder.push(item['price']));
-    priceHolder.sort();
-    priceHolder.reverse();
-    return Math.max(...priceHolder);
-
+    /**Sort data descending by price value
+     * then slice result from beginning to limit.
+     */
+    return data.sort(function (a, b){return b.price - a.price}).slice(0,limit);
 }
 
 console.log('Example:')
@@ -19,17 +16,24 @@ console.log(biggerPrice(2, [
     {"name": "water", "price": 1}
 ]))
 
-// assert.deepEqual(biggerPrice(2, [
-//     {"name": "bread", "price": 100},
-//     {"name": "wine", "price": 138},
-//     {"name": "meat", "price": 15},
-//     {"name": "water", "price": 1}
-// ]), [
-//     {"name": "wine", "price": 138},
-//     {"name": "bread", "price": 100}
-// ])
-// assert.deepEqual(biggerPrice(1, [
-//     {"name": "pen", "price": 5},
-//     {"name": "whiteboard", "price": 170}
-// ]), [{"name": "whiteboard", "price": 170}])
+console.log(biggerPrice(1, [
+    {"name": "bread", "price": 100},
+    {"name": "wine", "price": 138},
+    {"name": "meat", "price": 15},
+    {"name": "water", "price": 1}
+]))
+
+deepEqual(biggerPrice(2, [
+    {"name": "bread", "price": 100},
+    {"name": "wine", "price": 138},
+    {"name": "meat", "price": 15},
+    {"name": "water", "price": 1}
+]), [
+    {"name": "wine", "price": 138},
+    {"name": "bread", "price": 100}
+])
+deepEqual(biggerPrice(1, [
+    {"name": "pen", "price": 5},
+    {"name": "whiteboard", "price": 170}
+]), [{"name": "whiteboard", "price": 170}])
 console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
