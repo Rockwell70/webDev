04658e7d570518ff737bd6bc4bae44d03926ba17
Rockwell70 <rockwellshabani@gmail.com>
2022-07-04 16:49:17 -0700

    film finder work

--- javascript/filmFinder/public/script.js
+++ javascript/filmFinder/public/script.js
@@ -2,9 +2,31 @@ const tmdbKey = 'c0ffb381d49a8e2459f662fcb21a2efb';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
-    const genreRequestEndpoint = '/genre/movie/list'
+const getGenres = async () => {
+    const genreRequestEndpoint = '/genre/movie/list';
+    const requestParams = `?api_key=${tmdbKey}`;
+    const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+    //const urlToFetch = 'https://api.themoviedb.org/3/movie/550?api_key=c0ffb381d49a8e2459f662fcb21a2efb'
+
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+        } else {
+            // Handle server error
+            // example: INTERNAL SERVER ERROR: 500 error
+            console.log(`${response.statusText}: ${response.status} error`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+
+
 };
+
+getGenres();
 
 const getMovies = () => {
     const selectedGenre = getSelectedGenre();
