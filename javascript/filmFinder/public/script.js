560e8100e14ff750bf132db71011dfa67c442c2d
Rockwell70 <rockwellshabani@gmail.com>
2022-07-08 05:51:23 -0700

    between markers work

--- javascript/filmFinder/public/script.js
+++ javascript/filmFinder/public/script.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 const tmdbKey = 'c0ffb381d49a8e2459f662fcb21a2efb';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
@@ -6,13 +8,14 @@ const getGenres = async () => {
     const genreRequestEndpoint = '/genre/movie/list';
     const requestParams = `?api_key=${tmdbKey}`;
     const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
-    //const urlToFetch = 'https://api.themoviedb.org/3/movie/550?api_key=c0ffb381d49a8e2459f662fcb21a2efb'
 
     try {
         const response = await fetch(urlToFetch);
         if (response.ok) {
             const jsonResponse = await response.json();
-            console.log(jsonResponse);
+            const genres = jsonResponse.genres
+            //console.log(genres)
+            return genres
         } else {
             // Handle server error
             // example: INTERNAL SERVER ERROR: 500 error
