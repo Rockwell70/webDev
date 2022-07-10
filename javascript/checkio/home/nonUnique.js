59539acaaab740f06bd9b5cb4506cbfad098fe8d
Rockwell70 <rockwellshabani@gmail.com>
2022-07-10 06:24:01 -0700

    non uniques solved

--- javascript/checkio/home/nonUnique.js
+++ javascript/checkio/home/nonUnique.js
@@ -1,40 +1,38 @@
 import deepEqual from "assert";
 
 function nonUniqueElements(data) {
-  const counts = {};
-  let val_sum = 0;
-  const uniques = new Set(data).size;
-  for (const num of data) {
-    counts[num] = counts[num] ? counts[num] + 1 : 1;
-  }
-
-  for (const [key, value] of Object.entries(counts)) {
-    // console.log(`Key: ${typeof(key)} value ${typeof(value)}`)
-    val_sum += value;
-  }
-
-  // console.log(`Keys: ${uniques} vals sum ${val_sum}`)
-
-  if (val_sum === uniques) {
-    return [];
-  } else {
+    let counts = {};
+    let val_sum = [];
+    let result = [];
+    const equalsOne = (currentValue) => currentValue === 1;
     for (const num of data) {
-      if (counts[num] === 1) {
-        data.splice(data.indexOf(num), 1);
-      }
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
     }
-  }
-  return data;
+
+    for (const [, value] of Object.entries(counts)) {
+        val_sum.push(value);
+    }
+    if (val_sum.every(equalsOne)) {
+        return [];
+    } else {
+        for (const num of data) {
+            if (counts[num] !== 1) {
+                result.push(num);
+            }
+        }
+    }
+    return result;
 }
 
 console.log("Example:");
+console.log(nonUniqueElements([10, 20, 30, 10]));
 console.log(nonUniqueElements([1, 2, 3, 1, 3]));
 console.log(nonUniqueElements([1, 2, 3, 4, 5]));
 //
-// deepEqual(nonUniqueElements([1, 2, 3, 1, 3]), [1, 3, 1, 3]);
-// deepEqual(nonUniqueElements([1, 2, 3, 4, 5]), []);
-// deepEqual(nonUniqueElements([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5]);
-// deepEqual(nonUniqueElements([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9]);
+deepEqual(nonUniqueElements([1, 2, 3, 1, 3]), [1, 3, 1, 3]);
+deepEqual(nonUniqueElements([1, 2, 3, 4, 5]), []);
+deepEqual(nonUniqueElements([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5]);
+deepEqual(nonUniqueElements([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9]);
 console.log(
-  "Coding complete? Click 'Check' to review your tests and earn cool rewards!"
+    "Coding complete? Click 'Check' to review your tests and earn cool rewards!"
 );
