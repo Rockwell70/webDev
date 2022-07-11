fbed51dfbdd0e9bb9858ed575b51c04b3863038a
Rockwell70 <rockwellshabani@gmail.com>
2022-07-11 06:15:50 -0700

    non uniques solved

---
+++ javascript/checkio/home/popularWords.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/** @param {string} text
+ * @param {Array} words
+ * @return {dictionary} Number of occurrences of each word on words
+ */
+function popularWords(text, words) {
+    // your code here
+    return {};
+}
+
+
+console.log('Example:')
+console.log(popularWords(`
+When I was One
+I had just begun
+When I was Two
+I was nearly new`, ['i', 'was', 'three', 'near']))
