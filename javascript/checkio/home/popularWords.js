dcbcdc547c0f7ad52c323d56ac7ca1c19e105747
Rockwell70 <rockwellshabani@gmail.com>
2022-07-11 18:07:25 -0700

    non uniques solved

--- javascript/checkio/home/popularWords.js
+++ javascript/checkio/home/popularWords.js
@@ -5,8 +5,36 @@
  * @return {dictionary} Number of occurrences of each word on words
  */
 function popularWords(text, words) {
-    // your code here
-    return {};
+    let counts = {};
+    let countKeys = [];
+    //line break regex
+    const regex = /\n/gm;
+    /**Convert text to lowercase, remove leading/trailing spaces
+     * and replace line breaks with a single space
+     */
+    let textNoBreak = text.toLowerCase().trim().replace(regex, ' ');
+    //Split text into a list of words
+    let textWords = textNoBreak.split(' ');
+    //Add textWords word count to counts
+    for (const word of textWords) {
+        if (words.includes(word)) {
+            counts[word] = counts[word] ? counts[word] + 1 : 1;
+        }
+    }
+    //Create list of counts dictionary values
+    for (const [key,] of Object.entries(counts)) {
+        countKeys.push(key);
+    }
+    /**Add words list words to count dictionary
+     * with a value of zero if word is not in
+     * count dictionary.
+     */
+    words.forEach(word => {
+        if (!countKeys.includes(word)) {
+            counts[word] = 0;
+        }
+    });
+    return counts
 }
 
 
