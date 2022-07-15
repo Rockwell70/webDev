28bf8fb133ed8417d6ff80bb9000d5ef1630a430
Rockwell70 <rockwellshabani@gmail.com>
2022-07-15 06:17:11 -0700

    non uniques solved

---
+++ javascript/checkio/home/sortByFreq.js
@@ -0,0 +1,18 @@
+import * as assert from "assert";
+
+function frequencySort(items){
+  // your code here
+  return [];
+}
+
+console.log('Example:');
+console.log(frequencySort([4, 6, 2, 2, 6, 4, 4, 4]));
+
+// These "asserts" are used for self-checking and not for an auto-testing
+assert.deepEqual(frequencySort([4, 6, 2, 2, 6, 4, 4, 4]), [4, 4, 4, 4, 6, 6, 2, 2]);
+assert.deepEqual(frequencySort(['bob', 'bob', 'carl', 'alex', 'bob']), ['bob', 'bob', 'bob', 'carl', 'alex']);
+assert.deepEqual(frequencySort([17, 99, 42]), [17, 99, 42]);
+assert.deepEqual(frequencySort([]), []);
+assert.deepEqual(frequencySort([1]), [1]);
+
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
