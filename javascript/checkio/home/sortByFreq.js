35ed9fdafcde8eae33ea74cde34d18394d0cf22f
Rockwell70 <rockwellshabani@gmail.com>
2022-07-18 18:06:58 -0700

    non uniques solved

--- javascript/checkio/home/sortByFreq.js
+++ javascript/checkio/home/sortByFreq.js
@@ -7,47 +7,40 @@ import * as assert from "assert";
  **/
 function frequencySort(items) {
   let frequency = {},
-    sortAble = [],
-    newArr = [];
+    newArr = [],
+    arrSet = new Set();
+
+  items.forEach((num) =>{
+    arrSet.add(num.toString(10));
+  })
+
   items.forEach((item) => {
     frequency[item] = frequency[item] ? frequency[item] + 1 : 1;
   });
 
+  arrSet.forEach((num) => {
+    for (let i = 0; i < frequency[num]; i++) {
+      num.match(/\d/g) ?
+        newArr.push(parseInt(num)) :
+        newArr.push(num);
+    }
+  })
+
   if (items.length <= 1 || new Set(items).size === items.length) {
     return items;
-    // } else if (new Set(items).size === items.length) {
-    //   return items;
   } else {
-    for (let key in frequency) {
-      sortAble.push([key, frequency[key]]);
-    }
-
-    // sortAble.sort(function (a, b) {
-    //   return b[0] - a[0];
-    // });
-
-    sortAble.sort(function (a, b) {
-      return b[1] - a[1];
-    });
-
-    sortAble.forEach(function (obj) {
-      for (let i = 0; i < obj[1]; i++) {
-        obj[0].match(/\d/g)
-          ? newArr.push(parseInt(obj[0]))
-          : newArr.push(obj[0]);
-      }
-    });
     return newArr;
   }
 }
+
 // console.log(frequencySort([]))
 //console.log(frequencySort([4, 6, 2, 2, 6, 4, 4, 4]))
-//module.exports = frequencySort;
+// module.exports = frequencySort;
 
-// console.log('Example:');
-// console.log(frequencySort([4, 6, 2, 2, 6, 4, 4, 4]));
-//
-// // These "asserts" are used for self-checking and not for an auto-testing
+console.log('Example:');
+console.log(frequencySort([4, 6, 2, 2, 6, 4, 4, 4]));
+
+// These "asserts" are used for self-checking and not for an auto-testing
 assert.deepEqual(frequencySort([4, 6, 2, 2, 6, 4, 4, 4]), [4, 4, 4, 4, 6, 6, 2, 2]);
 assert.deepEqual(frequencySort(['bob', 'bob', 'carl', 'alex', 'bob']), ['bob', 'bob', 'bob', 'carl', 'alex']);
 assert.deepEqual(frequencySort([17, 99, 42]), [17, 99, 42]);
