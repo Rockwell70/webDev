a9b78ee291bc9a1db6d8d6f8c8927f30d7ed2b30
Rockwell70 <rockwellshabani@gmail.com>
2022-07-25 17:11:58 -0700

    axios parser

--- javascript/feeds/axios-rss.js
+++ javascript/feeds/axios-rss.js
@@ -1,1 +1,14 @@
-url = 'https://www.axios.com/feeds/feed.rss'
+// const fetch = require('node-fetch');
+import fetch from "node-fetch";
+
+const url = 'https://www.axios.com/feeds/feed.rss?format=xml';
+
+async function fetchAndParse() {
+  const response = await fetch(url);
+  const data = await response.text();
+  const parser = new DOMParser();
+  const XMLDocument = parser.parseFromString(data, 'text/xml')
+  console.log(XMLDocument);
+}
+
+fetchAndParse();
